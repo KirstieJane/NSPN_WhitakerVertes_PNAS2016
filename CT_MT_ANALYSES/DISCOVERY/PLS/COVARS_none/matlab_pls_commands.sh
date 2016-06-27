e06f92edbb36f214d201dcd277df9e724c595cbe
Kirstie Whitaker <kw401@cam.ac.uk>
2016-06-27 08:47:52 +0100

    Deleted files from master

--- CT_MT_ANALYSES/DISCOVERY/PLS/COVARS_none/matlab_pls_commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Not running PLS as it has already been run
-# Remove PLS2_ROIscores.csv file if you would like to overwrite the results
-# Not running PLS as it has already been run
-# Remove PLS_stats.csv file if you would like to overwrite the results
-# Not running candidate gene analysis as it has already been run
-# Remove PLS2_schizophrenia.csv file if you would like to overwrite the results
